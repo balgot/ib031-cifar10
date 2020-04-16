aee389279e0d1520bfc907bf4ac00b6e1eec1abc
vargac <simon.varga123@gmail.com>
2020-04-16 20:54:58 +0200

    EDA: Show also histograms of real images
    * it means not of average ones, but rather take some subsample
      from each category
    * Also print size of dataset
    
    Signed-off-by: vargac <simon.varga123@gmail.com>

--- src/EDA.py
+++ src/EDA.py
@@ -19,6 +19,16 @@ def img_for_show(raw_img):
 if __name__ == '__main__':
     batch = utils.load_data_batch(1)
     meta = utils.load_meta()
+    labels = meta[b'label_names']
+
+    """
+    Print dataset size info about each category
+    """
+    for i in range(10):
+        imgs = imgs_of_cat(batch, i)
+        print("Category {}: {}".format(i, labels[i]))
+        print("Size: {}".format(imgs.shape[0]))
+        print()
 
     """
     Find average image and plot histogram of RGB values for each category
@@ -34,12 +44,35 @@ if __name__ == '__main__':
         x, y = i % 5, (i // 5) * 2
 
         axs[y][x].imshow(img_for_show(avg_img.astype('int')))
-        axs[y][x].set_title(meta[b'label_names'][i])
+        axs[y][x].set_title(labels[i])
         axs[y][x].get_yaxis().set_visible(False)
         axs[y][x].get_xaxis().set_visible(False)
         sns.distplot(R, hist_kws=hist_kws, color='red',   ax=axs[y + 1][x])
         sns.distplot(G, hist_kws=hist_kws, color='green', ax=axs[y + 1][x])
         sns.distplot(B, hist_kws=hist_kws, color='blue',  ax=axs[y + 1][x])
 
+    plt.show()
+
+    """
+    Histogram of RGB values from whole batch. Take all values into account
+    instead of making an average image. But because whole dataset is too large,
+    we use only random subsample of size 100 images from each category.
+    """
+
+    fig, axs = plt.subplots(2, 5, figsize=(15, 8))
+    hist_kws = { 'range' : (0, 255) }
+
+    for i in range(10):
+        imgs = imgs_of_cat(batch, i)
+        imgs = imgs[np.random.choice(imgs.shape[0], 100)]
+        R = imgs[:,     :1024].reshape(-1)
+        G = imgs[:, 1024:2048].reshape(-1)
+        B = imgs[:, 2048:    ].reshape(-1)
+        x, y = i % 5, i // 5
+
+        axs[y][x].set_title(labels[i])
+        sns.distplot(R, hist_kws=hist_kws, color='red',   ax=axs[y][x])
+        sns.distplot(G, hist_kws=hist_kws, color='green', ax=axs[y][x])
+        sns.distplot(B, hist_kws=hist_kws, color='blue',  ax=axs[y][x])
 
     plt.show()
