f5b4f8d2876d4cc4fce72a7b0021d7635644730f
Michal Barnisin <xbarnis@fi.muni.cz>
2020-04-18 21:46:11 +0200

    Add comments, fix pep8, import graphs

--- src/hello_world.py
+++ src/hello_world.py
@@ -1,10 +1,14 @@
 from matplotlib import pyplot as plt
 import utils
+import graphs
 
+
+# Simple demo to demonstrate plotting graphs
 if __name__ == '__main__':
-    print("hello world")
+    # Load first data-batch
     imgs, labels = utils.read_data_batch(1)
-    utils.plot_random(imgs, labels, 3, 3,
-            fontsize='small', sharey=True, sharex=True, figsize=(3, 3))
+    # Plot 3 * 3 = 9 random images
+    graphs.plot_random(imgs, labels, 3, 3, fontsize='small',
+                       sharey=True, sharex=True, figsize=(3, 3))
     plt.tight_layout()
     plt.show()
