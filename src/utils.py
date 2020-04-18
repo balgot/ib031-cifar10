01a6a9f7126ada8608c0649761f29474fded72c2
Michal Barnisin <xbarnis@fi.muni.cz>
2020-04-18 21:28:33 +0200

    Restyle comments, add typing information, fix spelling mistakes

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,8 @@
-from matplotlib import pyplot as plt
-import seaborn as sns
 import numpy as np
+from typing import Dict, Any, Tuple
 
-"""
-Default path to untared image data
-"""
+
+# Default path to un-tarred image data
 CIFAR_PATH = '../dataset'
 
 """
@@ -26,114 +24,100 @@ a Python dictionary object. It has the following entries:
   label_names[0] == "airplane", label_names[1] == "automobile", etc.
 """
 
-def unpickle(file):
+
+def unpickle(path: str) -> Dict:
     """
     Loads data from file as is suggested on original cifar website
-    :param file: file to read
+
+    :param path: name of file to read
     :return: dict (format specified in global docs)
     """
     import pickle
-    with open(file, 'rb') as fo:
-        dict = pickle.load(fo, encoding='bytes')
-    return dict
+    with open(path, 'rb') as fo:
+        data_dict = pickle.load(fo, encoding='bytes')
+    return data_dict
 
-def load_meta(path=CIFAR_PATH):
-    return unpickle(path + '/batches.meta')
 
-def load_data_batch(i, path=CIFAR_PATH):
-    return unpickle(path + '/data_batch_' + str(i))
-
-def load_test_batch(path=CIFAR_PATH):
-    return unpickle(path + '/test_batch')
-
-def read_data_batch(i, path=CIFAR_PATH):
+def load_meta(path: str = CIFAR_PATH, filename: str = '/batches.meta') -> Dict:
     """
+    Load meta data from pickled file.
+
+    :param path: path to file-folder
+    :param filename: name of file with pickled meta data
+    :return: dictionary with metadata
+    """
+    return unpickle(path + filename)
+
+
+def load_data_batch(i: int, path: str = CIFAR_PATH, filename: str = '/data_batch_') -> Dict:
+    """
+    Load i-th data batch.
+
+    :param i: which batch to load
+    :param path: path to folder with file
+    :param filename: name of file with pickled data
+    :return: dictionary with loaded data
+    """
+    return unpickle(path + filename + str(i))
+
+
+def load_test_batch(path: str = CIFAR_PATH, filename: str = '/test_batch') -> Dict:
+    """
+    Load test batch.
+
+    :param path: path to folder with file
+    :param filename: name of file with pickled data
+    :return: dictionary with loaded data
+    """
+    return unpickle(path + filename)
+
+
+def read_data_batch(i: int, path: str = CIFAR_PATH) -> Tuple[Any, np.ndarray]:
+    """
+    Load and process data-batch file.
+
+    :param i: which batch to load
+    :param path: path to folder with file
     :return: tuple (imgs, labels) of np.arrays (note: integer labels)
     """
     batch = load_data_batch(i, path)
     return batch[b'data'], np.array(batch[b'labels'])
 
-def read_test_batch(i, path=CIFAR_PATH):
+
+def read_test_batch(path: str = CIFAR_PATH) -> Tuple[Any, np.ndarray]:
     """
+    Load and process test file.
+
+    :param path: path to folder with file
     :return: tuple (imgs, labels) of np.arrays (note: integer labels)
     """
-    batch = load_test_batch(i, path)
+    batch = load_test_batch(path)
     return batch[b'data'], np.array(batch[b'labels'])
 
-def read_meta(path=CIFAR_PATH):
+
+def read_meta(path: str = CIFAR_PATH) -> np.ndarray:
     """
-    :return: np.array of label names
+    Loads and returns CIFAR10 label names in order, i.e.
+    to label 0, 0-th string name corresponds.
+
+    :param path: path to meta folder
+    :return: np.array of label names (of type np.bytes_)
     """
     return np.array(load_meta(path)[b'label_names'])
 
-def get_lname(label):
+
+def get_label_name(label: int) -> str:
     """
     :param label: integer image label
     :return: corresponding string label name
     """
     return read_meta()[label].decode('utf-8')
 
-def imgs_of_cat(batch, category):
+
+def imgs_of_cat(batch: Dict, category: int) -> np.ndarray:
     """
     :param batch: From where to obtain images
     :param category: Images of what category to obtain
     :return: np.array of all images of specific category (e.i. label) 
     """
     return batch[b'data'][np.array(batch[b'labels']) == category]
-
-def img_for_show(raw_img):
-    """
-    Creates new image prepared to show from raw data from batch
-    :param raw_img: 1D format of image
-    :return: 3D format (y, x, rgb) ready for pyplot's imshow
-    """
-    return raw_img.reshape((32, 32, 3), order='F').swapaxes(0, 1)
-
-def plot_raw_img(raw_img, label, ax, fontsize='medium'):
-    """
-    Plots single image on specified ax (see Axes).
-    :param raw_img: format as in batch (1D array [R, G, B])
-    :param label: integer label (None for no title)
-    :param ax: ax to plot on
-    :param fontsize: passed to ax.set_title
-    """
-    ax.imshow(img_for_show(raw_img))
-    if label is not None:
-        ax.set_title(f"{label}: {get_lname(label)}", fontsize=fontsize)
-    ax.get_yaxis().set_visible(False)
-    ax.get_xaxis().set_visible(False)
-
-def plot_RGB_hist(raw_imgs, ax, **kwargs):
-    """
-    Plots histograms of image(s)'s R, G, B amount using seaborn
-    :param raw_imgs: array of images (1D format) or just one such image
-    :param ax: ax on which to plot
-    :param **kwargs: passed to seaborn.distplot
-    """
-    R = raw_imgs.reshape((-1, 3072))[:,     :1024].reshape(-1)
-    G = raw_imgs.reshape((-1, 3072))[:, 1024:2048].reshape(-1)
-    B = raw_imgs.reshape((-1, 3072))[:, 2048:    ].reshape(-1)
-    sns.distplot(R, color='red',   ax=ax, **kwargs)
-    sns.distplot(G, color='green', ax=ax, **kwargs)
-    sns.distplot(B, color='blue',  ax=ax, **kwargs)
-
-
-def plot_random(imgs, labels, nrows=1, ncols=1, fontsize='medium', **kwargs):
-    """
-    Plots random images from batch using matplotlib pyplot.
-    User should typically call pyplot.show() after this function
-
-    :param imgs: images in form 1D array [Rvalue, Gvalues, Bvalues]
-    :param labels: labels (numerical) attached to images
-    :param nrows: number of rows of images
-    :param ncols: number of cols of images
-    :param fontsize: passed to pyplot.Axes.set_title
-    :param **kwargs: passed to pyplot.subplots
-    """
-    fig, axs = plt.subplots(nrows, ncols, **kwargs)
-
-    for ax in axs.reshape(-1):
-        random = np.random.randint(0, imgs.shape[0])
-        img = imgs[random]
-        label = labels[random]
-        plot_raw_img(img, label, ax, fontsize=fontsize)
