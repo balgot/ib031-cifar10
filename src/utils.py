faa76dea9f2c93b8d608175ebe9421e1c448442c
Michal Barnisin <xbarnis@fi.muni.cz>
2020-04-24 17:48:00 +0200

    Add newlines, remove <utils.> error

--- src/utils.py
+++ src/utils.py
@@ -85,6 +85,7 @@ def read_data_batch(i: int, path: str = CIFAR_PATH) -> Tuple[Any, np.ndarray]:
     batch = load_data_batch(i, path)
     return batch[b'data'], np.array(batch[b'labels'])
 
+
 def read_dataset(path: str = CIFAR_PATH) -> Tuple[Any, np.ndarray]:
     """
     Load and process (concatenate) all data-batch files.
@@ -94,12 +95,13 @@ def read_dataset(path: str = CIFAR_PATH) -> Tuple[Any, np.ndarray]:
     """
     all_images, all_labels = [], []
     for i in range(1, 6):
-        imgs, labels = utils.read_data_batch(i)
+        imgs, labels = read_data_batch(i)
         all_images.append(imgs)
         all_labels.append(labels)
     all_images = np.concatenate(all_images, axis=0)
     all_labels = np.concatenate(all_labels, axis=0)
     return all_images, all_labels
+
 
 def read_test_batch(path: str = CIFAR_PATH) -> Tuple[Any, np.ndarray]:
     """
