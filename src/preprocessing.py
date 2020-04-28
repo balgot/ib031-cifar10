01f08540c423a93a51a39424f99d6d064f033dad
Michal Barnisin <xbarnis@fi.muni.cz>
2020-04-28 22:41:05 +0200

    Add edges detection from skimage

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -2,7 +2,7 @@ import numpy as np
 import utils
 from skimage.color import rgb2gray, rgb2hsv
 from skimage.feature import hog
-from skimage import exposure
+from skimage import filters
 import matplotlib.pyplot as plt
 
 
@@ -40,59 +40,75 @@ def rgb_to_hsv(batch: np.ndarray) -> np.ndarray:
 
 if __name__ == "__main__":
     X, y = utils.read_data_batch(1)
+    # !NORMALISE
     X = batch_to_rgb(X) / 255.0
 
     pic = X[1545]
-    gray = rgb_to_gray(pic)
-    hsv = rgb_to_hsv(pic)
-    hue = hsv[:, :, 0]
-    sat = hsv[:, :, 1]
-    val = hsv[:, :, 2]
-
-    fig, axes = plt.subplots(3, 3, figsize=(8, 4))
+    fig, axes = plt.subplots(4, 3, figsize=(8, 4))
     ax = axes.ravel()
+
     ax[0].imshow(pic)
     ax[0].set_title("Original")
+    print(f"Original = {pic.shape}")
 
+    gray = rgb_to_gray(pic)
     ax[1].imshow(gray, cmap=plt.cm.gray)
     ax[1].set_title("Gray")
+    print(f"Gray = {gray.shape}")
 
     # Now to transform whole array
     grays = rgb_to_gray(X)
     ax[2].imshow(grays[1545], cmap=plt.cm.gray)
     ax[2].set_title("Gray - array")
 
+    hsv = rgb_to_hsv(pic)
+    hue = hsv[:, :, 0]
+    sat = hsv[:, :, 1]
+    val = hsv[:, :, 2]
     ax[3].imshow(hue, cmap='hsv')
     ax[3].set_title("Hue")
-
     ax[4].imshow(sat, cmap='hsv')
     ax[4].set_title("Saturation")
-
     ax[5].imshow(val, cmap='hsv')
     ax[5].set_title("Value")
+    print(f"HSV = {hsv.shape}")
 
     # HOG
     fd, hog_image = hog(pic, orientations=8, pixels_per_cell=(8, 8),
                         cells_per_block=(1, 1), visualize=True, multichannel=True)
     ax[6].imshow(hog_image, cmap=plt.cm.gray)
     ax[6].set_title("Hog")
+    print(f"Hog = {fd.shape}")  # 128 = (32 * 32) / (8 * 8) * orientations[=8]
 
     # hog detailed
-    _, hog_det = hog(pic, pixels_per_cell=(4, 4), cells_per_block=(1, 1),
+    fd_det, hog_det = hog(pic, pixels_per_cell=(4, 4), cells_per_block=(1, 1),
                      visualize=True, multichannel=True)
     ax[7].imshow(hog_det, cmap=plt.cm.gray)
     ax[7].set_title("Hog detailed")
+    # print(fd_det.shape)  # 576 = (32 * 32) / (4 * 4) * orientations[=9]
 
     # hog more detailed
     _, hog_det_ = hog(pic, pixels_per_cell=(2, 2), cells_per_block=(1, 1),
-                     visualize=True, multichannel=True)
+                      visualize=True, multichannel=True)
     ax[8].imshow(hog_det_, cmap=plt.cm.gray)
     ax[8].set_title("Hog more detailed")
 
-    print(pic.shape)
-    print(gray.shape)
-    print(hsv.shape)
-    print(hog_image.shape)
+    # edges detection
+    sobel = filters.sobel(rgb_to_gray(pic))
+    ax[9].imshow(sobel)
+    ax[9].set_title("Sobel (edges)")
+    print(f"Sobel = {sobel.shape}")
+
+    roberts = filters.roberts(rgb_to_gray(pic))
+    ax[10].imshow(roberts)
+    ax[10].set_title("Roberts (edges)")
+    print(f"Roberts = {roberts.shape}")
+
+    prewitt = filters.prewitt(rgb_to_gray(pic))
+    ax[11].imshow(prewitt)
+    ax[11].set_title("Prewitt (edges)")
+    print(f"Prewitt = {prewitt.shape}")
+    print(prewitt)
 
     fig.tight_layout()
     plt.show()
