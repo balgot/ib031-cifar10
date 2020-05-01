2106fb7a38772bca0dfcbc5cf9413892911d422f
Michal Barnisin <xbarnis@fi.muni.cz>
2020-05-01 16:33:03 +0200

    Fix cmap= argument

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -75,13 +75,13 @@ def demo(X: np.ndarray, ax: np.ndarray) -> None:
     print(f"Original = {pic.shape}")
 
     gray = rgb_to_gray(pic)
-    ax[1].imshow(gray, cmap=plt.cm.gray)
+    ax[1].imshow(gray, cmap='gray')
     ax[1].set_title("Gray")
     print(f"Gray = {gray.shape}")
 
     # Now to transform whole array
     grays = rgb_to_gray(X)
-    ax[2].imshow(grays[1545], cmap=plt.cm.gray)
+    ax[2].imshow(grays[1545], cmap='gray')
     ax[2].set_title("Gray - array")
 
     hsv = rgb_to_hsv(pic)
@@ -90,48 +90,47 @@ def demo(X: np.ndarray, ax: np.ndarray) -> None:
     val = hsv[:, :, 2]
     ax[3].imshow(hue, cmap='hsv')
     ax[3].set_title("Hue")
-    ax[4].imshow(sat, cmap='hsv')
+    ax[4].imshow(sat, cmap='binary')
     ax[4].set_title("Saturation")
-    ax[5].imshow(val, cmap='hsv')
+    ax[5].imshow(val, cmap='gray')
     ax[5].set_title("Value")
     print(f"HSV = {hsv.shape}")
 
     # HOG
     fd, hog_image = hog(pic, orientations=8, pixels_per_cell=(8, 8),
                         cells_per_block=(1, 1), visualize=True, multichannel=True)
-    ax[6].imshow(hog_image, cmap=plt.cm.gray)
+    ax[6].imshow(hog_image, cmap='gray')
     ax[6].set_title("Hog")
     print(f"Hog = {fd.shape}")  # 128 = (32 * 32) / (8 * 8) * orientations[=8]
 
     # hog detailed
     fd_det, hog_det = hog(pic, pixels_per_cell=(4, 4), cells_per_block=(1, 1),
                           visualize=True, multichannel=True)
-    ax[7].imshow(hog_det, cmap=plt.cm.gray)
+    ax[7].imshow(hog_det, cmap='gray')
     ax[7].set_title("Hog detailed")
     # print(fd_det.shape)  # 576 = (32 * 32) / (4 * 4) * orientations[=9]
 
     # hog more detailed
     _, hog_det_ = hog(pic, pixels_per_cell=(2, 2), cells_per_block=(1, 1),
                       visualize=True, multichannel=True)
-    ax[8].imshow(hog_det_, cmap=plt.cm.gray)
+    ax[8].imshow(hog_det_, cmap='gray')
     ax[8].set_title("Hog more detailed")
 
     # edges detection
     sobel = filters.sobel(rgb_to_gray(pic))
-    ax[9].imshow(sobel)
+    ax[9].imshow(sobel, cmap='gray')
     ax[9].set_title("Sobel (edges)")
     print(f"Sobel = {sobel.shape}")
 
     roberts = filters.roberts(rgb_to_gray(pic))
-    ax[10].imshow(roberts)
+    ax[10].imshow(roberts, cmap='gray')
     ax[10].set_title("Roberts (edges)")
     print(f"Roberts = {roberts.shape}")
 
     prewitt = filters.prewitt(rgb_to_gray(pic))
-    ax[11].imshow(prewitt)
+    ax[11].imshow(prewitt, cmap='gray')
     ax[11].set_title("Prewitt (edges)")
     print(f"Prewitt = {prewitt.shape}")
-    print(prewitt)
 
 
 if __name__ == "__main__":
